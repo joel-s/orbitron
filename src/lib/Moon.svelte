3e08154d1cf307d84812587647b65ad3feec3285
Joel Sullivan <joel@jsullivan.us>
2025-05-11 13:55:01 -0400

    Set up repo with help from "npx sv create"

---
+++ src/lib/Moon.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import '../app.css';
+
+	let { size, angle, offset } = $props();
+</script>
+
+<style>
+    .ball {
+        position: absolute;
+        border-radius: 50%;
+        background: #000;
+    }
+</style>
+
+<div
+	class="ball"
+	style:width="{size}vmin"
+	style:height="{size}vmin"
+	style:transform="rotateZ({angle}deg) translateX({offset}vmin)"
+></div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import Moon from '$lib/Moon.svelte';
+
+	const nMoons = 3;
+	const angle = 360 / nMoons;
+	let iVals = [...Array(nMoons).keys()];
+</script>
+
+<style>
+    .page {
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .center {
+				width: 0;
+				height: 0;
+        position: absolute;
+        animation: rotateAnimation 10s linear infinite;
+    }
+
+    @keyframes rotateAnimation {
+        from {
+            transform: rotate(0deg);
+        }
+        to {
+            transform: rotate(360deg);
+        }
+    }
+</style>
+
+<div class="page">
+	<div class="center">
+		{#each iVals as iVal (iVal)}
+			<Moon size={5} angle={angle * iVal} offset={30} />
+		{/each}
+	</div>
+</div>
