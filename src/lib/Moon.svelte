57a978839b5aaf7d0427e2ca1549338684ce1bf4
Joel Sullivan <joel@jsullivan.us>
2025-05-31 10:31:48 -0400

    Implement spokes (switch B)

--- src/lib/Moon.svelte
+++ src/lib/Moon.svelte
@@ -2,6 +2,7 @@
   import '../app.css';
   import Moons from '$lib/Moons.svelte';
   import { moonState } from '$lib/moonState.svelte';
+  import { switches } from '$lib/switchValues.svelte';
 
   let { index, angle, offset, size, color } = $props();
 
@@ -19,13 +20,17 @@
   ];
 
   const colorName = $derived(colors[color]);
+  const lineBackground = $derived(switches.revealSpokes && offset > 0 ? 'var(--retro6)' : 'none');
 </script>
 
-<div class="origin" style:transform="rotateZ({angle}deg) translateY(-{offset}vmin)">
-  <div class="ball" style:width="{size}vmin" style:height="{size}vmin" style:background-color={colorName}></div>
-  {#if index < moonState.length - 1}
-    <Moons index={index + 1} />
-  {/if}
+<div class="origin" style:transform="rotateZ({angle}deg)">
+  <div class="line" style="height: {offset}vmin; background: {lineBackground}"></div>
+  <div class="offset" style:transform="translateY(-{offset}vmin)">
+    <div class="ball" style:width="{size}vmin" style:height="{size}vmin" style:background-color={colorName}></div>
+    {#if index < moonState.length - 1}
+      <Moons index={index + 1} />
+    {/if}
+  </div>
 </div>
 
 <style>
@@ -33,6 +38,14 @@
     position: absolute;
     width: 0;
     height: 0;
+  }
+
+  .line {
+    position: absolute;
+    bottom: 0;
+    width: 1px;
+    background-color: var(--retro8);
+    z-index: -100;
   }
 
   .ball {
